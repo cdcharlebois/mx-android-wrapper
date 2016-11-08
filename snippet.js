cc4d05df22e57de612ba6b9fd0acdf9a83415e3c
Conner <cdcharlebois@gmail.com>
2016-11-08 10:42:32 -0500

    added comments to snippet.js

--- snippet.js
+++ snippet.js
@@ -4,9 +4,9 @@
   ,   barcode = document.querySelector(sel).value
   // var barcode = "90210" // get from somewhere in the dom -- success
   // var barcode = "999" // get from somewhere in the dom -- fail
-  ,   mxscope = this
   ,   offlinePage = page + ".page.xml"
 
+  // query the sqlite db to see if there's a matching record
   mx.data.getSlice("DLAM.CageCard", [
     {
       attribute: "card_id",
@@ -18,18 +18,21 @@
     offset:0,
     limit:1,
     sort: []
-  }, // limit/offset/sort
+  }, // limit/offset/sort (for some reason this is required)
   function(res, count){
     // if the record exists...
-    if (count > 0){
+    if (count === 1){
       var thisRecord = res[0]
       ,   newContext = new mendix.lib.MxContext()
       ,   cardGuid   = thisRecord.getGuid();
+
+      // create a new OfflineAHC, and tie it to the existing CageCard
       mx.data.create({
         entity: "DLAM.OfflineAHC",
         callback: function(obj){
-          obj.set("DLAM.OfflineAHC_CageCard", cardGuid)
+          obj.set("DLAM.OfflineAHC_CageCard", cardGuid) // set association
           newContext.setContext(obj)
+          // then, navigate to the new page, with the new OfflineAHC as context
           mx.ui.openForm(offlinePage, {
               location: "content",
               context: newContext
